a3fb1261fa7ca901489fe7b3a31a9ed5f3073a90
Luis Fernando <lsh.fernandors@gmail.com>
2021-02-03 20:34:48 -0300

    adicionando infinite page

--- js/index.js
+++ js/index.js
@@ -1,33 +1,95 @@
 
 $(document).ready(function () {
-    console.log('pronto!!');
-    carregarPokemons();
+    localStorage.setItem('page', '1')
 });
 
-async function carregarPokemons() {
-    const pokemons = await getData('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0');
+$(window).scroll(function () {
+    if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+        
+      let page = Number(localStorage.getItem('page'));
+
+      if (page) {
+          page += 1;
+          carregarPokemons(page)
+      }
+    }
+});
+
+async function carregarPokemons(page) {
+    
+    const offset = page > 1 ? page * 100 : 0;
+
+    const pokemons = await getData(`https://pokeapi.co/api/v2/pokemon?limit=100&offset=${offset}`);
 
     const arrPromisse = pokemons.results.map(poke => {
         return getData(poke.url)
     });
 
-    const requestAll = await Promise.all(arrPromisse)
-
-    console.log(requestAll)
+    const requestAll = await Promise.all(arrPromisse);
 
     const list = document.getElementById('pokeList');
 
-    let htmlList = '';
+    // let htmlList = '';
 
     requestAll.map(poke => {
-        htmlList += `
-            <div class='pokemon ${poke.types[0].type.name}'>
-                <img src="${poke.sprites.other['official-artwork'].front_default}" alt="">            
-                <p>${poke.name}</p>
-            </div>
-        `
+
+        var div = document.createElement('div');
+
+        div.setAttribute('class', `pokemon ${poke.types[0].type.name}`);
+
+
+        var imagem = document.createElement('img');
+        var nome = document.createElement('p');
+
+        nome.innerHTML = poke.name;
+
+        imagem.setAttribute('src', poke.sprites.other['official-artwork'].front_default);
+
+        div.appendChild(imagem);
+        div.appendChild(nome);
+
+        list.appendChild(div);
+        // htmlList += `
+        //     <div class='pokemon ${poke.types[0].type.name}'>
+        //         <img src="${poke.sprites.other['official-artwork'].front_default}" alt="">            
+        //         <p>${poke.name}</p>
+        //     </div>
+        // `
     });
-    list.innerHTML = htmlList;
+    // list.innerHTML = htmlList;
+
+    if (page > 1) {
+        localStorage.setItem('page', page);
+    }
+}
+
+async function carregarMeusPokemons() {
+
+    const myListPokemons = await localStorage.getItem('pokemons');
+
+    let html = '';
+
+    if (!myListPokemons) {
+        html = `
+            <p>Ainda não existe nenhum pokemon na sua pokedex</p>
+        `
+    }
+    else {
+        html = `
+            <p>Tem pokemon</p>
+        `;
+    }
+
+    const list = document.getElementById('meusPokes');
+
+    list.innerHTML = html;
+
+    localStorage.setItem('poke', JSON.stringify({ teste: true }));
+}
+
+async function addPokemon() {
+    const pokes = localStorage.getItem('poke');
+    console.log(JSON.parse(pokes));
 }
 
 function getData(ajaxurl) {
