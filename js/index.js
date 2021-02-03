c2103c71a4a930c081a62c25a1a2720ecf5090fe
Luis Fernando <lsh.fernandors@gmail.com>
2021-02-03 00:32:09 -0300

    adicionando chamadas e estilizando

--- js/index.js
+++ js/index.js
@@ -1,13 +1,42 @@
-function teste() {
 
+$(document).ready(function () {
+    console.log('pronto!!');
+    carregarPokemons();
+});
+
+async function carregarPokemons() {
+    const pokemons = await getData('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0');
+
+    const arrPromisse = pokemons.results.map(poke => {
+        return getData(poke.url)
+    });
+
+    const requestAll = await Promise.all(arrPromisse)
+
+    console.log(requestAll)
+
+    const list = document.getElementById('pokeList');
+
+    let htmlList = '';
+
+    requestAll.map(poke => {
+        htmlList += `
+            <div class='pokemon ${poke.types[0].type.name}'>
+                <img src="${poke.sprites.other['official-artwork'].front_default}" alt="">            
+                <p>${poke.name}</p>
+            </div>
+        `
+    });
+    list.innerHTML = htmlList;
 }
 
-function buscarInfo() {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", "https://pokeapi.co/api/v2/pokemon/bulbasaur", false); // false for synchronous request
-    xmlHttp.send();
-    console.log(xmlHttp.responseText);
+function getData(ajaxurl) {
+    return $.ajax({
+        url: ajaxurl,
+        type: 'GET',
+    });
 }
+
 
 function abrirMenu() {
     document.getElementById("mySidebar").style.width = "250px";
